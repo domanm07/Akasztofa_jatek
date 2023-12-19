2f9e6738b5731615a4071ee606e6210b0a4dc903
Domán <domanmartin07@gmail.com>
2023-12-19 14:19:29 +0100

    próba3

--- game.py
+++ game.py
@@ -8,7 +8,21 @@ wordTemplate = list()
 
 word = randomWordGenerate()
 #print(word)
+#list-comprehension ehelyett jobb lesz.
 for i in range(len(word)):
     wordTemplate.append("_")
  
-print(" ".join(wordTemplate))   
+print()
+print(" ".join(wordTemplate))
+
+print("\nTaláld ki a gondolt szót! 10 lehetőséged van. hogy megadj egy-egy karaktert.")
+tip = input("Az ön tippje: ")
+
+if tip not in word:
+    print("Hát ez bizony rossz ötlet volt.")
+else:
+    for i in range(len(word)):
+        if word[i] == tip:
+            wordTemplate[i] = tip 
+
+print(" ".join(wordTemplate)) 
